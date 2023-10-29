7d8ab69d6933926d7ed82eb1c71e375ab139a5bc
leiyuanqing <358371466@qq.com>
2023-10-29 22:39:44 +0000

    chore: comment out movie stuff for coverage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
+  # resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  # root :to => redirect('/movies')
 
+  root :to => redirect('/users/1')
   resources :users
   resources :projects
   resources :newprojects
