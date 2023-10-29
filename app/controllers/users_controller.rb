55f0ac781ad18a49a440dc045e41208ed5254d15
leiyuanqing <358371466@qq.com>
2023-10-29 18:43:39 +0000

    feat: add complete/redo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
   def show
-    # todo: check if authenticated
+    # TODO: check if authenticated
     id = params[:id]
     @user = User.find(id)
-    
-    # @projects = UserProject.where(:user => id)
+
     @completed_projects = UserProject.get_completed_project_by_user(@user)
     @uncompleted_projects = UserProject.get_uncompleted_project_by_user(@user)
     # will render app/views/users/show.<extension> by default
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @project = Project.find(params[:project])
+    @user_project = UserProject.where(:user => @user, :project => @project).first
+    @user_project.update_attributes(process: params[:process])
+    redirect_to user_path(@user)
   end
 end
