d332793d14b6fba864a252b4d9e87b11d1898ac9
ws2694 <ws2694@columbia.edu>
2023-11-15 00:57:08 +0000

    100% coverage for user_project.rb

--- spec/models/user_project_spec.rb
+++ spec/models/user_project_spec.rb
@@ -19,4 +19,31 @@ RSpec.describe UserProject, type: :model do
     UserProject.create(:user => user, :project => project, :process => 100)
     expect(UserProject.where(:user_id => user, :project_id => project, :process => 100).count).to eq(1)
   end
+  describe '.get_uncompleted_project_by_user' do
+    it 'returns uncompleted projects for a given user' do
+      user = User.create(name: 'user1')
+      project1 = Project.create(name: 'project1')
+      project2 = Project.create(name: 'project2')
+      UserProject.create(user: user, project: project1, process: 50)
+      UserProject.create(user: user, project: project2, process: 100)
+
+      uncompleted_projects = UserProject.get_uncompleted_project_by_user(user)
+      expect(uncompleted_projects.length).to eq(1)
+      expect(uncompleted_projects.first.project_id).to eq(project1.id)
+    end
+  end
+
+  describe '.get_completed_project_by_user' do
+    it 'returns completed projects for a given user' do
+      user = User.create(name: 'user1')
+      project1 = Project.create(name: 'project1')
+      project2 = Project.create(name: 'project2')
+      UserProject.create(user: user, project: project1, process: 100)
+      UserProject.create(user: user, project: project2, process: 50)
+
+      completed_projects = UserProject.get_completed_project_by_user(user)
+      expect(completed_projects.length).to eq(1)
+      expect(completed_projects.first.project_id).to eq(project1.id)
+    end
+  end
 end
